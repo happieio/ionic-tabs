c3fdb7a9c5323f53cf9ac74559ef9a22e96707d3
James Spencer <thisjustosay@gmail.com>
2017-05-01 02:09:25 -0700

    initial demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { Demos } from '../pages/demos/controller';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DeviceMotion } from '@ionic-native/device-motion';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    Demos,
     ContactPage,
     HomePage,
     TabsPage
@@ -26,7 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    Demos,
     ContactPage,
     HomePage,
     TabsPage
@@ -34,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    DeviceMotion,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/pages/demos/controller.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {DeviceMotion, DeviceMotionAccelerationData} from "@ionic-native/device-motion";
+
+@Component({
+  selector: 'demos',
+  templateUrl: 'view.html'
+})
+export class Demos {
+
+  private motion_sub = null;
+  read_x = 0;
+  read_y = 0;
+  read_z = 0;
+
+  constructor(public navCtrl: NavController, private deviceMotion: DeviceMotion) {
+
+  }
+
+  ionViewDidEnter(){
+    this.motion_sub = this.deviceMotion.watchAcceleration().subscribe((acceleration: DeviceMotionAccelerationData) => {
+      this.read_x = acceleration.x;
+      this.read_y = acceleration.y;
+      this.read_z = acceleration.z;
+    });
+  }
+
+  ionViewWillLeave(){
+    this.motion_sub.unsubscribe();
+  }
+}
